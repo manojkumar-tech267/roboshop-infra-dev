066dc060d143b5157afcca135ba42beaf76dca1e
manojkumar-tech267 <manojkumarparuchuri8@gmail.com>
2026-06-24 19:05:35 +0530

    made some changes for production

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,8 +3,11 @@
 dnf install ansible -y
 
 component=$1
+environment=$2
+
 cd /home/ec2-user
 git clone https://github.com/manojkumar-tech267/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
-ansible-playbook -e component=$component roboshop.yaml
+git pull
+ansible-playbook -e component=$component -e environment=$environment roboshop.yaml
--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -3,6 +3,8 @@ locals {
     database_subnet_id = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
+    mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+    rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
     common_tags = {
         Project = var.project
         Environment = var.environment
@@ -20,4 +22,18 @@ locals {
         },
         local.common_tags
     )
+    mysql_final_tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-mysql"
+        },
+        local.common_tags
+    )
+    rabbitmq_final_tags = merge(
+        {
+            Name = "${var.project}-${var.environment}-rabbitmq"
+        },
+        local.common_tags
+    )
+    mysql_role_name = join("-",[ for name in ["${var.project}","${var.environment}","mysql"] : title(name) ])
+    mysql_policy_name = join("",[ for name in ["${var.project}","${var.environment}","mysql"] : title(name) ])
 }
